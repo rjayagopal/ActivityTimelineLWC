3c476345be9ff72ba815fce354fb8fb33e1f0b9d
WCMillerJLG <wmiller@johnsonlawgroup.co.uk>
2026-03-27 10:57:29 -0400

    commit to fix missing email from addresses

--- force-app/main/default/lwc/timelineItemTask/timelineItemTask.js
+++ force-app/main/default/lwc/timelineItemTask/timelineItemTask.js
@@ -65,6 +65,7 @@ export default class TimelineItemTask extends NavigationMixin(LightningElement) 
     @track toAddresses;
     @track ccAddresses;
     @track firstRecipient;
+    @track fromAddress;
     @api fieldData;
     @api displayRelativeDates;
     @api taskClosedStatus;
@@ -125,7 +126,13 @@ export default class TimelineItemTask extends NavigationMixin(LightningElement) 
         if (data) {
             if (data.ActivityId) {
                 // its data from the EmailMessage object
+                if (data.FromName == null || String(data.FromName).trim() === '') {
+                    this.fromAddress = data.FromAddress;
+                } else {
+                    this.fromAddress = undefined;
+                }
                 this.assignedToName=data.FromName;
+
                 this.description=data.TextBody;
                 this.recordId=data.Id;
                 this.activityId=data.ActivityId;
@@ -219,6 +226,27 @@ export default class TimelineItemTask extends NavigationMixin(LightningElement) 
 
     get isEmail(){
         return this.taskSubtype === "Email";
+    }
+
+    get emailSenderDisplayName() {
+        if (this.assignedToName != null && String(this.assignedToName).trim() !== '') {
+            return this.assignedToName;
+        }
+        return this.fromAddress;
+    }
+
+    get emailSenderUsesFromAddressOnly() {
+        if (this.assignedToName != null && String(this.assignedToName).trim() !== '') {
+            return false;
+        }
+        return this.fromAddress != null && String(this.fromAddress).trim() !== '';
+    }
+
+    get fromAddressMailtoHref() {
+        if (!this.fromAddress) {
+            return '';
+        }
+        return `mailto:${this.fromAddress}`;
     }
 
     get hasWhoTo(){
